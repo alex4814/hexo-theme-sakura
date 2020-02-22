866b533b6a7af4334fce4ea268d4ef6b29a26ad6
alex4814 <alex4814@126.com>
2020-02-22 12:41:36 +0800

    Site vendors displaying in footer, namely dmit cloudflare in the time being

--- themes/Sakura/scripts/filter/locals.js
+++ themes/Sakura/scripts/filter/locals.js
@@ -3,5 +3,15 @@
 hexo.extend.filter.register('template_locals', locals => {
     const { env } = hexo;
     locals.hexo_version = env.version;
+    locals.vendors = {
+        dmit: {
+            url: 'https://www.dmit.io/',
+            logo: '/images/logo/dmit-blue.svg',
+        },
+        cloudflare: {
+            url: 'https://www.cloudflare.com/',
+            logo: '/images/logo/cf-logo-v.svg',
+        }
+    }
     return locals;
 });
